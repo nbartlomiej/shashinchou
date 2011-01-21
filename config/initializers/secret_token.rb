0b0662d91b4de8b536ceae26ddbce29d5b6dcb0c
Bartlomiej N <nbartlomiej@gmail.com>
2011-01-21 21:16:44 +0100

    empty rails directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shashinchou::Application.config.secret_token = '8df2add820afaff2a082f2c47115bbfc46aeb0c81cacaba3263badc878faf11d3077dc3e5cbedcdeb03cd7a2540103b0e247d052d40ddf9d2bd7da49dfc67519'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Shashinchou::Application.config.session_store :cookie_store, :key => '_shashinchou_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Shashinchou::Application.config.session_store :active_record_store
