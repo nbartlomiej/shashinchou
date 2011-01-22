9cab8ff5ffdcca2bf54d9ae036daa5acf1e1da5f
Bartlomiej N <nbartlomiej@gmail.com>
2011-01-23 00:10:16 +0100

    paperclip photo scaling, thumbnails, with tests

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :product
-  has_attached_file :image
+  has_attached_file :image, :styles => {:large => '1024x768>', :thumbnail => '256x256#'}, :default_style => :large
 
   validates_attachment_presence :image
 end
--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -5,11 +5,11 @@ class PhotoTest < ActiveSupport::TestCase
   # Replace this with your real tests.
 
   test "should save simple photo" do
-    assert Photo.new(:image => generate_test_image).save
+    assert Photo.new(:image => fixture_png_upload).save
   end
 
   test "should calculate photo properties" do
-    p = Photo.new(:image => generate_test_image)
+    p = Photo.new(:image => fixture_png_upload)
     p.save
     # properties that should be auto-updated
     # by paperclip whenever an image is supplied
@@ -24,22 +24,45 @@ class PhotoTest < ActiveSupport::TestCase
     assert !Photo.new().save
   end
 
+  test "images greater than 1024 are scaled" do
+    image_path = "images/big.png"
+    # checking locally whether the image file
+    # is indeed above 1024 - so that scaling is
+    # really tested; a hack, works only on pngs
+    # see: http://stackoverflow.com/questions/2450906/is-there-a-simple-way-to-get-image-dimensions-in-ruby
+    dimensions = IO.read("#{Rails.root}/test/fixtures/#{image_path}")[0x10..0x18].unpack('NN') # [width, height]
+    assert dimensions[0] > 1024
+    assert dimensions[1] > 768
+
+    photo = Photo.new(:image => fixture_png_upload(image_path))
+    photo.save
+    # fetching the image in its default style
+    geometry = Paperclip::Geometry.from_file(photo.image.path)
+
+    # has the image been properly scaled?
+    assert geometry.width <= 1024
+    assert geometry.height <= 768
+  end
+
   # meta-test for the private method for generating 
   # test images (below); if this fails, other tests
   # may behave incorrectly (dependancies)
   test "private test image generator works" do
     assert_nothing_raised do
-      assert_not_nil generate_test_image
-      assert_not_equal generate_test_image, generate_test_image
+      assert_not_nil fixture_png_upload
+      assert_not_nil fixture_png_upload 'images/big.png'
+      assert_not_equal fixture_png_upload, fixture_png_upload
     end
   end
 
   private
 
   # generates test image, uses a sample png shipped
-  # with rails (copied to the fixture directory)
-  # todo: broaden and probably simulate multiple extensions
-  def generate_test_image
-    fixture_file_upload("rails.png", "image/png")
+  # with rails (copied to the fixture directory) or 
+  # any other png file with custom path
+  def fixture_png_upload path="rails.png"
+    # creates a mock uploaded file (for testing file upload...),
+    # will search for files in the ./test/fixtures directory
+    fixture_file_upload(path, "image/png")
   end
 end
