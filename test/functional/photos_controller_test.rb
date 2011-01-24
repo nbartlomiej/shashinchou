350699e3d522fb70f6e65f20ff1e6ebb0281d60c
Bartlomiej N <nbartlomiej@gmail.com>
2011-01-24 16:11:36 +0100

    updated photo views, controller, tweaked tests

--- test/functional/photos_controller_test.rb
+++ test/functional/photos_controller_test.rb
@@ -5,17 +5,6 @@ class PhotosControllerTest < ActionController::TestCase
     @photo = photos(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:photos)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create photo" do
     assert_difference('Photo.count') do
       post :create, :photo => @photo.attributes
@@ -27,16 +16,6 @@ class PhotosControllerTest < ActionController::TestCase
   test "should show photo" do
     get :show, :id => @photo.to_param
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @photo.to_param
-    assert_response :success
-  end
-
-  test "should update photo" do
-    put :update, :id => @photo.to_param, :photo => @photo.attributes
-    assert_redirected_to photo_path(assigns(:photo))
   end
 
   test "should destroy photo" do
